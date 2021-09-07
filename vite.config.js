e9ee7a9aaaf6c1d30226d8af081123a8a5c5833e
Tosn <taoserking@163.com>
2021-09-07 16:05:13 +0800

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue-calendar',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src')
